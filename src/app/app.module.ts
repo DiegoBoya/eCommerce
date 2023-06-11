fce9bd181bbe4b4f81e86ad0c8253fabd2265afd
Diego Boyatjian <dboyatjian@gmail.com>
2023-06-11 19:00:18 -0300

    creacion de ProducService, refactorizacion del codigo, saco logica del componente y lo llevo al servicio, consumo de API con httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
@@ -22,7 +22,8 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
